bfe89563023ecedea0ed5b4735f626cf6be7ccf2
Tung Duong <duongthanhtungvn01@gmail.com>
2020-07-19 15:15:58 +0100

    Update

--- app/urls_api.py
+++ app/urls_api.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views_api import *
 urlpatterns = [
-    path('hello', hello)
+    path('hello', hello),
+    path('get_category_list', getCategoryList),
+    path('search_product', searchProduct),
 ]
--- app/views_api.py
+++ app/views_api.py
@@ -4,3 +4,26 @@ from rest_framework.decorators import api_view
 @api_view(['GET'])
 def hello(request):
     return Response({'message': 'Hello'})
+
+from .models import *
+from .serializers import *
+from .views_user import findProducts
+
+@api_view(['GET'])
+def searchProduct(request):
+    #productList = Product.objects.all()
+    productName = request.GET.get('product_name', '')
+    categoryId = request.GET.get('category_id', '')
+    categoryId = int(categoryId) if categoryId != '' else 0
+    priceRange = request.GET.get('price_range', '')
+    priceRange = int(priceRange) if priceRange != '' else 0
+    
+    productList = findProducts(productName, categoryId, priceRange)
+    data = ProductSerializer(productList, many=True).data
+    return Response({'productList': data})
+
+@api_view(['GET'])
+def getCategoryList(request):
+    categoryList = Category.objects.all()    
+    data = CategorySerializer(categoryList, many=True).data
+    return Response({'categoryList': data})
